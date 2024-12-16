e3ffda40f8c06c7d109c0daa4f586c47454cf780
Martinelii <Kevenmartineli@outlook.com>
2024-12-16 13:55:48 -0300

    Model, Repository, Controller & Relacionamento

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,78 @@
+package com.generation.farmacia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(min = 1, max = 50, message = "Nome do produto deve conter entre 1 a 50 caracteres")
+	private String nome;
+	
+	@NotBlank
+	@Size(min = 5, max = 100, message = "Descricao do produto deve conter entre 5 e 100 caracteres")
+	private String descricao;
+	
+	@NotNull(message = "Preço não pode ser vazio")
+	private Long preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Long getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Long preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+}
